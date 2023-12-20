1824a9b45e1b30697f177bf4e5626daeb0324e19
Jin <boj924@compute-e-16-236.o2.rc.hms.harvard.edu>
2023-12-20 16:08:01 -0500

    fix indel sig

--- R/gatk_processing.R
+++ R/gatk_processing.R
@@ -194,7 +194,8 @@ annotate.gatk <- function(gatk, genome.string, add.mutsig=TRUE) {
             mutsig := get.3mer(chr=chr, pos=pos, refnt=refnt, altnt=altnt,
                                genome=genome.string.to.bsgenome.object(genome.string))]
 	       
-        gatk_indel <- gatk[muttype == 'indel'] 
+        gatk_indel <- gatk[muttype == 'indel'] 	
+        if (nrow(gatk_indel) == 0) { next }
 
 #	for(i in 1:nrow(gatk_indel)) {
 #	    chr=gatk_indel$chr[i]
@@ -212,19 +213,14 @@ annotate.gatk <- function(gatk, genome.string, add.mutsig=TRUE) {
 #        #chs <- classify.indels(gatk[muttype == 'indel'], genome.string=genome.string)
 #        gatk[muttype == 'indel', mutsig := chs_list]
 
-	chs_list <- rep('NA',nrow(gatk_indel))
         chs_list <- lapply(1:nrow(gatk_indel), function(i) {
 	    chr=gatk_indel$chr[i]
 	    pos=gatk_indel$pos[i]
 	    refnt=gatk_indel$refnt[i]
 	    altnt=gatk_indel$altnt[i]
 
-            # added due to weird error in NA in tabix file
-            if ( (!is.character(refnt)) | (!is.character(altnt)) ) { "NA" }
-
 	    chs=attribution_of_indels(genome=genome.string.to.bsgenome.object(genome.string),
 				in_CHROM=chr, in_POS=pos, in_REF=refnt, in_ALT=altnt)
-	    chs
         })
         gatk[muttype == 'indel', mutsig := unlist(chs_list)] 
 
